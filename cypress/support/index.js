d09ef763e939c95aad415f42308793ef9f9cef69
kafziel4 <keoma4@gmail.com>
2020-12-24 14:55:06 -0300

    test: report update

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -22,3 +22,16 @@ import './commands'
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false
 })
+
+import addContext from 'mochawesome/addContext'
+
+Cypress.on('test:after:run', (test, runnable) => {
+    if(test.state == 'failed') {
+        const screenshotFileName = `${runnable.parent.title} -- ${test.title} (failed).png`;
+        addContext({ test }, `assets/${Cypress.spec.name}/${screenshotFileName}`);
+
+        let videoName = Cypress.spec.name
+        videoName = `${videoName.replace('/.js.*', 'js')}.mp4`
+        addContext({ test }, `videos/${videoName}`);
+    }
+})
